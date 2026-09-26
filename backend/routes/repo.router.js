dca3723029f330468771f2b449d819f7bf3b8d47
vivek02-xo <vivek02.xo@gmail.com>
2026-09-26 16:03:39 +0530

    Correcting name of controller.

--- backend/routes/repo.router.js
+++ backend/routes/repo.router.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const userController = require("../controllers/repoController.js");
+const repoController = require("../controllers/repoController.js");
 const repoRouter = express.Router();
 
-repoRouter.post("/repo/create", userController.createRepository);
-repoRouter.get("/repo/all", userController.getAllRepositories);
-repoRouter.get("/repo/:id", userController.fetchRepositoryById);
-repoRouter.get("/repo/:name", userController.fetchRepositoryByName);
-repoRouter.get("/repo/:userID", userController.fetchRepositoryForCurrentUser);
-repoRouter.put("/repo/update/:id", userController.updateRepositoryById);
-repoRouter.delete("/repo/delete/:id", userController.deleteRepositoryById);
-repoRouter.patch("repo/toggle/:id", userController.toggleRepositoryById);
+repoRouter.post("/repo/create", repoController.createRepository);
+repoRouter.get("/repo/all", repoController.getAllRepositories);
+repoRouter.get("/repo/:id", repoController.fetchRepositoryById);
+repoRouter.get("/repo/:name", repoController.fetchRepositoryByName);
+repoRouter.get("/repo/:userID", repoController.fetchRepositoryForCurrentUser);
+repoRouter.put("/repo/update/:id", repoController.updateRepositoryById);
+repoRouter.delete("/repo/delete/:id", repoController.deleteRepositoryById);
+repoRouter.patch("repo/toggle/:id", repoController.toggleRepositoryById);
 
 module.exports = repoRouter;
